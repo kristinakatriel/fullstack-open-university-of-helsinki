27f703185d7b3dd2fc472c8f84fd9d7d06dcacd7
kristinakatriel <kristinakc.nunag@gmail.com>
2025-04-16 21:07:51 +1000

    ex 1.14 done, part 1 now done

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -4,13 +4,36 @@ const DisplayVotes = (props) => {
   console.log(props)
   return (
     <div>
-      Has {props.num} votes.
+      Has {props.num} {props.num === 1 ? 'vote' : 'votes'}.
     </div>
   )
 }
+
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>{text}</button>
 )
+
+const DisplayHighlyVoted = (props) => {
+  console.log(props)
+  console.log(Math.max(...props.array))
+  console.log(props.array.indexOf(Math.max(...props.array)))
+  console.log(props.anecdotes[props.array.indexOf(Math.max(...props.array))])
+  const votedAnecdote = props.anecdotes[props.array.indexOf(Math.max(...props.array))]
+  const numVotes = Math.max(...props.array)
+  if (numVotes == 0) {
+    return (
+      <div>
+        No votes have been cast yet.
+      </div>
+    )
+  }
+  return (
+    <div>
+      {votedAnecdote}<br /><br />
+      Has {numVotes} {numVotes === 1 ? 'vote' : 'votes'}.
+    </div>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -43,10 +66,13 @@ const App = () => {
 
   return (
     <div>
-      {anecdotes[selected]}<br />
-      <DisplayVotes num={array[selected]}/>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}<br /><br />
+      <DisplayVotes num={array[selected]}/><br />
       <Button onClick={handleVoteClicked} text="Vote" />
       <Button onClick={handleNextClicked} text="Next Anecdote" />
+      <h1>Anecdote with most votes</h1>
+      <DisplayHighlyVoted array={array} anecdotes={anecdotes}/>
     </div>
   )
 }
