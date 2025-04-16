083c3a4fa531e95e6cc9ec2e16c3bda8890b29ed
kristinakatriel <kristinakc.nunag@gmail.com>
2025-04-16 18:08:15 +1000

    ex 1.4 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,7 +2,7 @@ const Header = (props) => {
   console.log(props)
   return (
     <div>
-      <h1>{props.name}</h1>
+      <h1>{props.course}</h1>
     </div>
   )
 }
@@ -25,33 +25,37 @@ const Content = ({ parts }) => {
 
 const Total = (props) => {
   console.log(props)
+  // NB at this point you can assume that there are always three items,
+  let total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises 
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {total}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <div>
-      <Header name={course}/>
-      <Content parts={[part1, part2, part3]} />
-      <Total total={10 + 7 + 14}/>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
