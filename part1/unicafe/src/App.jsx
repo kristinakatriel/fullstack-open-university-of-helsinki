71f50fe070ff3cf4fec0358dddb64eda64a95212
kristinakatriel <kristinakc.nunag@gmail.com>
2025-04-16 19:49:21 +1000

    ex 1.8 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,11 +7,27 @@ const Display = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Statistics:</h1>
+      <Display name='Good'counter={props.good}/>
+      <Display name='Neutral'counter={props.neutral}/>
+      <Display name='Bad'counter={props.bad}/>
+      <Display name='All'counter={props.all}/>
+      <Display name='Average'counter={props.average}/>
+      <Display name='Positive'counter={props.positive} isPercentage={true}/>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
   const [all, setAll] = useState(0)
   const [average, setAverage] = useState(0)
   const [positive, setPositive] = useState(0)
@@ -45,27 +61,21 @@ const App = () => {
     setAverage((good - newBad) / newAll)
     setPositive((good / newAll) * 100)
   }
-
   
   return (
     <div>
       <h1>Give Feedback!</h1>
-      <button onClick={handleGoodClicked}>
-        Good
-      </button>
-      <button onClick={handleNeutralClicked}> 
-        Neutral
-      </button>
-      <button onClick={handleBadClicked}> 
-        Bad
-      </button>
-      <h1>Statistics:</h1>
-      <Display name='Good'counter={good}/>
-      <Display name='Neutral'counter={neutral}/>
-      <Display name='Bad'counter={bad}/>
-      <Display name='All'counter={all}/>
-      <Display name='Average'counter={average}/>
-      <Display name='Positive'counter={positive} isPercentage={true}/>
+      <button onClick={handleGoodClicked}>Good</button>
+      <button onClick={handleNeutralClicked}>Neutral</button>
+      <button onClick={handleBadClicked}>Bad</button>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      />
     </div>
   )
 }
