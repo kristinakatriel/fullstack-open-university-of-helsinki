d9fef9571ddec97df0c219c7fa406b926dcf38b1
kristinakatriel <kristinakc.nunag@gmail.com>
2025-04-30 19:08:19 +1000

    moved logic for displaying info into a new file

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,4 +1,5 @@
 import Search from './components/Search'
+import Content from './components/Content'
 import countryService from './services/countries'
 import { useState, useEffect } from 'react'
 
@@ -13,40 +14,6 @@ const App = () => {
   : countries.filter(country =>
       (country.name.common?.toLowerCase() ?? '').includes(search.toLowerCase())
     );
-
-  let content = null;
-
-  if (countriesFound.length > 10) {
-    content = <p>Too many matches, specify another filter</p>;
-  } else if (countriesFound.length === 1) {
-    console.log(countriesFound)
-    content = (
-      countriesFound.map(country => (
-        <div key={country.name.common}>
-          <h1>{country.name.common}</h1>
-          <div>Capital {country.capital}</div>
-          <div>Area {country.area}</div>
-          <h2>Languages</h2>
-          <ul>
-            {Object.values(country.languages).map((lang, index) => (
-              <li key={index}>{lang}</li>
-            ))}
-          </ul>
-          <img
-            src={country.flags.svg || country.flags.png}
-            alt={country.flags.alt}
-            width="150"
-          />
-        </div>
-      ))
-    );
-  } else if (countriesFound.length > 0) {
-    content = (
-      countriesFound.map(country => (
-        <div key={country.name.common}>{country.name.common}</div>
-      ))
-    );
-  }
 
   useEffect(() => {
     countryService
@@ -63,8 +30,8 @@ const App = () => {
 
   return (
     <div>
-      <Search search={search} handleSearchChange={handleSearchChange}/>
-      {content}
+      <Search search={search} handleSearchChange={handleSearchChange} />
+      <Content countriesFound={countriesFound} />
     </div>
   )
 }
