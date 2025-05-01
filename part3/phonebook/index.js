f67304598433c189ad04c7aa760a0ff544e98884
kristinakatriel <kristinakc.nunag@gmail.com>
2025-05-01 15:30:48 +1000

    exercise 3.5 done, implemented functionality so that new phonebook entries can be added

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -55,6 +55,26 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    id: String(Math.random() * persons.length),
+    name: body.name || false,
+    number: body.number || '',
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
