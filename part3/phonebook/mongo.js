405f70881ecd2b17552c0d72d93a1462e279a3a5
kristinakatriel <kristinakc.nunag@gmail.com>
2025-05-03 17:55:06 +1000

    exercise 3.12 done, created a cloud-based MongoDB database for the phonebook application with MongoDB Atlas

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url = `mongodb+srv://kristinakatriel:${password}@cluster0.rbxjl6c.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Persons', personSchema)
+
+if (personName && personNumber) {
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`)
+    mongoose.connection.close()
+  })
+} else if (!personName || !personNumber) {
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
+
+
+
